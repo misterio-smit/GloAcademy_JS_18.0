87f38af2dee3d852ec6ee549ffaaf68f0f214d56
Igor <57453357+misterio-smit@users.noreply.github.com>
2021-04-03 14:16:20 +0700

    firs commit

---
+++ lesson1/js/lesson1.js
@@ -0,0 +1,14 @@
+"use strict";
+let money;
+let income; 
+let addExpenses; 
+let deposit;
+let mission; 
+let period;
+
+
+
+console.log('Все работает');
+ alert("Нажми 'ok' чтобы запустить код ");
+
+
